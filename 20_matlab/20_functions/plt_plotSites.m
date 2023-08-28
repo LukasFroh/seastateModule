b9d702be35e24e9995097198ecbe8ac6ea4c22f2
froehling <froehling@lufi.uni-hannover.de>
2023-08-28 15:15:23 +0200

    removed circle <o> for marking sites in plot

--- 20_matlab/20_functions/plt_plotSites.m
+++ 20_matlab/20_functions/plt_plotSites.m
@@ -1,7 +1,7 @@
 function plt_plotSites(siteData,validSitesIdx,siteMarkerSize,siteTextColor,fsAxis)
 
 % Plot available measuring sites with text
-scatter([siteData(validSitesIdx).lon],[siteData(validSitesIdx).lat],siteMarkerSize,siteTextColor,'o','LineWidth',1)
+% scatter([siteData(validSitesIdx).lon],[siteData(validSitesIdx).lat],siteMarkerSize,siteTextColor,'o','LineWidth',1)
 scatter([siteData(validSitesIdx).lon],[siteData(validSitesIdx).lat],siteMarkerSize,siteTextColor,'x','LineWidth',1)
 % Loop over all sites to plot different text colors
 for i = 1:numel(validSitesIdx)
