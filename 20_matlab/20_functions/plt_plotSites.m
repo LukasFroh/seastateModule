83941721de33d31cbb00d18bd5987caadaf5d18b
froehling <froehling@lufi.uni-hannover.de>
2023-08-28 18:26:15 +0200

    Site plot color green. Switched to gray if no data is available

--- 20_matlab/20_functions/plt_plotSites.m
+++ 20_matlab/20_functions/plt_plotSites.m
@@ -1,9 +1,25 @@
-function plt_plotSites(siteData,validSitesIdx,siteMarkerSize,siteTextColor,fsAxis)
+function plt_plotSites(siteData,validSitesIdx,siteMarkerSize,siteTextColor,siteTextColorNoData,fsAxis,siteScales)
 
-% Plot available measuring sites with text
-% scatter([siteData(validSitesIdx).lon],[siteData(validSitesIdx).lat],siteMarkerSize,siteTextColor,'o','LineWidth',1)
-scatter([siteData(validSitesIdx).lon],[siteData(validSitesIdx).lat],siteMarkerSize,siteTextColor,'x','LineWidth',1)
+% Intermediate variables
+lon     = [siteData(validSitesIdx).lon];
+lat     = [siteData(validSitesIdx).lat];
+names   = [siteData(:).name];
+
 % Loop over all sites to plot different text colors
 for i = 1:numel(validSitesIdx)
-    text([siteData(i).lon]+0.03,[siteData(i).lat],siteData(i).name,'FontSize',fsAxis,'Color',siteTextColor(i,:),'FontWeight','bold')
+    if strcmpi(names{i},'FN1')
+        horizDispl = -0.2;
+    else
+        horizDispl = +0.03;
+    end
+
+    if isnan(siteScales(i))
+        scatter(lon(i),lat(i),siteMarkerSize,siteTextColorNoData,'x','LineWidth',1)                               % Marker I 
+        % scatter(lon(i),lat(i),siteMarkerSize,siteTextColor,'o','LineWidth',1)                             % Marker II
+        text(lon(i)+horizDispl,lat(i),['$\it{' names{i} '}$'],'FontSize',fsAxis,'Color',siteTextColorNoData,'FontWeight','bold','Interpreter','latex')  % Site text
+    else
+        scatter(lon(i),lat(i),siteMarkerSize,siteTextColor(i,:),'x','LineWidth',1)                               % Marker I 
+        % scatter(lon(i),lat(i),siteMarkerSize,siteTextColor,'o','LineWidth',1)                             % Marker II
+        text(lon(i)+horizDispl,lat(i),names{i},'FontSize',fsAxis,'Color',siteTextColor(i,:),'FontWeight','bold')  % Site text
+    end
 end
