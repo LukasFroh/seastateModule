97b3ec9c9788a815cc43c030c546370c8a2158ad
froehling <froehling@lufi.uni-hannover.de>
2023-10-11 18:10:16 +0200

    changed version number in compile script

--- 20_matlab/10_scripts/compileSeastateMasterFnc.m
+++ 20_matlab/10_scripts/compileSeastateMasterFnc.m
@@ -6,7 +6,7 @@ fnctPath    = 'C:\Users\LuFI_LF\seadrive_root\froehlin\Meine Bibliotheken\GitLab
 scriptName  = 'seastateMasterFnc.m';
 % String formatting char("' ... '") mandatory due to blanks in path...
 outputFolder = char("'C:\Users\LuFI_LF\seadrive_root\froehlin\Meine Bibliotheken\GitLab\Seegangsmodul\30_execution'");
-outputName = 'seastateModule_v1_2';
+outputName = 'seastateModule_v1_3';
 
 % Change directory to script folder
 cd(scriptPath)
