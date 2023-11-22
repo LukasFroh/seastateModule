eae42ec4e9462546a0c825bd4387221710d6dfd0
froehling <lukas.froehling@outlook.com>
2023-11-22 16:05:44 +0100

    Updated paths

--- 20_matlab/10_scripts/compileSeastateMasterFnc.m
+++ 20_matlab/10_scripts/compileSeastateMasterFnc.m
@@ -1,11 +1,11 @@
 %% Compile master function into standalone .exe
 % Script path
-scriptPath  = 'C:\Users\LuFI_LF\seadrive_root\froehlin\Meine Bibliotheken\GitLab\Seegangsmodul\20_matlab\10_scripts';
+scriptPath  = 'C:\Users\LuFI_LF\OneDrive\LuFI\04_Projekte\03_OpenRAVE\45_Github\seastateModule\20_matlab\10_scripts';
 % Function path
-fnctPath    = 'C:\Users\LuFI_LF\seadrive_root\froehlin\Meine Bibliotheken\GitLab\Seegangsmodul\20_matlab\20_functions';
+fnctPath    = 'C:\Users\LuFI_LF\OneDrive\LuFI\04_Projekte\03_OpenRAVE\45_Github\seastateModule\20_matlab\20_functions';
 scriptName  = 'seastateMasterFnc.m';
 % String formatting char("' ... '") mandatory due to blanks in path...
-outputFolder = char("'C:\Users\LuFI_LF\seadrive_root\froehlin\Meine Bibliotheken\GitLab\Seegangsmodul\30_execution'");
+outputFolder = char("'C:\Users\LuFI_LF\OneDrive\LuFI\04_Projekte\03_OpenRAVE\45_Github\seastateModule\30_execution'");
 outputName = 'seastateModule_v1_3';
 
 % Change directory to script folder
